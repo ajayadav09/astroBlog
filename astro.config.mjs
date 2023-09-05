9adb709059475b17a4b01b6d335e5c41f557bfb4
Ajay Yadav <ajay.yadav@newfold.com>
2023-09-05 19:57:48 +0530

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ajayadav09.github.io',
+  base: '/astroBlog',
+})
